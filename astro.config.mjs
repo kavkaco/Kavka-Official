846e9cd9444d25fc949e4f6e8998aef7223762eb
[ Taha. Dostifam‍ ] <mr.tahadostifam@gmail.com>
2024-05-24 17:18:34 +0330

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), sitemap(), icon()],
-  output: "server",
-  adapter: vercel()
 });
