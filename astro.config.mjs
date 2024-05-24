28124b249587330b21ea433d4625b26841b0190b
[ Taha. Dostifam‍ ] <mr.tahadostifam@gmail.com>
2024-05-24 17:32:22 +0330

    add vercel domain to astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kavka.vercel.app/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
