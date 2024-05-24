da1fd1c6c5b358bc089acc056720600ae32f1426
[ Taha. Dostifam‍ ] <mr.tahadostifam@gmail.com>
2024-05-24 17:15:27 +0330

    update content and get ready to deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: "server",
+  adapter: vercel()
 });
