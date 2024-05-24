d3d65d253fd59f3246868346b9af31b2aa2dd416
[ Taha. Dostifam‍ ] <mr.tahadostifam@gmail.com>
2024-05-24 16:47:01 +0330

    basic design of website completed

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        lightGrey: "#141414",
+        primary: "#1759f3"
+      },
+      fontFamily: {
+        sans: [
+          "Bricolage Grotesque Variable",
+          "Inter Variable",
+          "Inter",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
